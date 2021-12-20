f8e1cfc2941db196b993b9931a69bd30ee6440da
Sara Oshana <59624406+saraoshana@users.noreply.github.com>
2021-12-19 22:08:57 -0500

    adding searching by element

--- scraping.py
+++ scraping.py
@@ -6,3 +6,7 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 driver.get("link for the website goes here")
+
+"""try to find element by id first because it is unique,
+if not found, try name, if not found, try class name """
+search = driver.find_element_by_name("s")
